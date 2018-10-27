6f0542d5ae70f25b8ffb96fb483c098fc38b4704
omprakash <tulala.omprakash123@gmail.com>
2018-10-27 09:02:19 +0530

    Add files via upload

---
+++ netflix-eureka-naming-sever/src/main/java/com/omprakash/microservices/netflixeurekanamingsever/NetflixEurekaNamingSeverApplication.java
@@ -0,0 +1,14 @@
+package com.omprakash.microservices.netflixeurekanamingsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaNamingSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingSeverApplication.class, args);
+	}
+}
